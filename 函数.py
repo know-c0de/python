acab6a98dc8bd7b1dc6656bfabb4342fd4ad94c5
know-c0de <code.sec01@gmail.com>
2013-11-12 18:04:25 +0800

    Create 函数.py

---
+++ 函数.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+def get_even(n):
+    sum = 0
+    for x in range(n):
+        if x % 2 is 0:
+            sum += n
+        return sum
+print get_even(10)
+print get_even(11)
+print get_even(100)
+print get_even(500)
