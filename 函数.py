14e1517e821abe5b8ed29ffc686c4bb1a6647848
know-c0de <code.sec01@gmail.com>
2013-11-12 18:09:04 +0800

    Update 函数.py

--- 函数.py
+++ 函数.py
@@ -3,10 +3,12 @@
 
 def get_even(n):
     sum = 0
+    
     for x in range(n):
         if x % 2 is 0:
             sum += x
         return sum
+
 print get_even(10)
 print get_even(11)
 print get_even(100)
