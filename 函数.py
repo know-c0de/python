f4366f3c334c886ca48b3af8a9bdba10a7443ab6
know-c0de <code.sec01@gmail.com>
2013-11-13 11:14:32 +0800

    Update 函数.py

--- 函数.py
+++ 函数.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-def get_even(n):
-    sum = 0
+def get_even(n):                  #定义函数，参数
+    sum = 0                       #初始化sum赋值
     
-    for x in range(n):
-        if x % 2 is 0:
-            sum += x
-        return sum
+    for x in range(n):            #循环，给定x的数值范围
+        if x % 2 is 0:            
+            sum += x              #判断x是否为偶数，执行sum = sum + x
+        return sum                
 
-print get_even(10)
-print get_even(11)
+print get_even(10)                #当参数为10，输出
+print get_even(11)                #当参数为11，输出
 print get_even(100)
 print get_even(500)
