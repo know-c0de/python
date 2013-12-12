e031d5a3ddec804205a0600aea489f9ed1e0ec9c
know-c0de <code.sec01@gmail.com>
2013-12-12 14:15:30 +0800

    Update getWebPage.py

--- getWebPage.py
+++ getWebPage.py
@@ -5,18 +5,25 @@
 import urllib2
 import sys
 
+url = []
+data = []
+
 
 def getWebPageContent(url):
     with urllib2.urlopen(url) as webpage:
         data = webpage.read()
     return data
 
-    if sys.argv[1] == url:
-        content = data
-        con = getWebPageContent()
-        con.feed(content)
-        print content
-        if not url.startswith("http://"):
-            url = "http:/"+url
-    else:
-        print 'Usage:%s url' % sys.argv[0]
+if url == sys.argv[1]:
+    content = data
+    con = getWebPageContent()
+    con.feed(content)
+    print content
+    if not url.startswith("http://"):
+        url = "http://"+url
+else:
+    print 'Usage:%s url' % sys.argv[0]
+
+
+if __name__ == '__main__':
+    getWebPageContent(url)
